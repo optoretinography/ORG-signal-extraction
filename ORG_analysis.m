df782968df778848977091f1157d3a8eed398e6d
Huakun-Li <huakunli17@gmail.com>
2025-09-25 13:55:54 +0800

    Correct typos

--- ORG_analysis.m
+++ ORG_analysis.m
@@ -1,12 +1,12 @@
 % Copyright (C) 2025  Huakun Li, Yueming Zhuo, Daniel Palanker
 %
 %   This script provides an example for extracting ORG signals in response
-%   to a flash sequence of green-UV-freen-UV.
-%   OCT recording: 4000 repeated B-scans at a 10 kHz frame rate
-%   Green flash: 1 ms, 340 μJ
-%   UV flash: 1 ms, 112 μJ
+%   to a flash sequence of green-UV-green-UV.
+%   OCT recording: 4000 repeated B-scans at a 10 kHz frame rate.
+%   Green flash: 1 ms, 340 μJ.
+%   UV flash: 1 ms, 112 μJ.
 %   Timing: pre-stimulus time was 10.04 ms (~100 frames), the interval
-%   between ajacent flashes was 100 ms
+%   between adjacent flashes was 100 ms.
 %
 %   This library is free software; you can redistribute it and/or
 %   modify it under the terms of the GNU Lesser General Public
@@ -44,9 +44,11 @@ end
 % setup path
 RegisteredDataFolder = ['./' RegisteredDataFileName '/']; % path to the registered data folder
 file_name = 'I_reg';
-FigSaveFolder = [RegisteredDataFolder 'figure/']; % path to the registered data folder
-if ~exist(FigSaveFolder, 'dir')
+FigSaveFolder = [RegisteredDataFolder 'figure/']; % folder to save exported figures
+ResultSaveFolder = [RegisteredDataFolder 'analysis/']; % folder to save ORG signals
+if ~(exist(FigSaveFolder, 'dir') && exist(ResultSaveFolder, 'dir'))
     mkdir(FigSaveFolder);
+    mkdir(ResultSaveFolder);
 end
 
 % load data
@@ -77,9 +79,9 @@ R_dX = 5; % for each pixel in the target layer, the reference region spans acros
 R_dZ = 2; 
 
 ORG_traces = zeros(N_x*(2*T_dZ+1), N_t); % ORG traces extracted from individual pixels in the target layer
-loc = zeros(N_x*(2*T_dZ+1), 2); % indice of individual pixels in the target layer
+loc = zeros(N_x*(2*T_dZ+1), 2); % indices of individual pixels in the target layer
 
-I_Tref = I_reg .* conj(repmat(I_reg(:,:,1), 1, 1, N_t)); % calcualte time referenced OCT signals
+I_Tref = I_reg .* conj(repmat(I_reg(:,:,1), 1, 1, N_t)); % calculate time referenced OCT signals
 processed_pixel_num = 0;
 for i = 1:N_x
     for j = -T_dZ:T_dZ
@@ -105,6 +107,7 @@ end
 ORG_phase_trace = angle(mean(exp(1i*ORG_traces), 1)); % average across all pixels in the target layer
 ORG_phase_trace = unwrap(ORG_phase_trace);
 ORG_OPL_trace = ORG_phase_trace * phase_to_OPL; % convert phase change into OPL change
+save([ResultSaveFolder, 'ORG_signal.mat'], 'samp_time', 'ORG_OPL_trace');
 
 %% Plot ORG signal 
 figure;
